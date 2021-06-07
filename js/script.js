81492a6b95c6350f3b5d13b58b558ad85c0b87bc
Roberto <robertoanfuso98@gmail.com>
2021-06-07 15:51:12 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var parola=prompt("inserisci parola palindroma");
+
+
+
+function contrpali(x){
+
+    for(var i=0;i<x.lenght() ; i++){
+        if(x[i]==x[(x.lenght()-i)]){
+            
+        }
+    }
+}
